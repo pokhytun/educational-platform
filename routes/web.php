aa2e67471a891929666d53b2d2a289f2611a2e7e
Pokhytun Andrii <pokhytunandrii@gmail.com>
2022-02-25 19:28:13 +0300

    add ui & auth packages

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Main'], function () {
     Route::get('/' , [IndexController::class, 'index']);
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
