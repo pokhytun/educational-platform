427cacb7b2c3da0d713986980a5781442ff1db2e
Pokhytun Andrii <pokhytunandrii@gmail.com>
2022-02-26 21:28:32 +0300

    add gate for permission

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\RolesServiceProvider::class,
+        App\Providers\PermissionServiceProvider::class,
 
     ],
 
